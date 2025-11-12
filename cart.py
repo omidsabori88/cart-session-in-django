8f10f5f67f179414edf2b6b43656392b0fc68a29
Omid <140541298+omidsabori88@users.noreply.github.com>
2025-11-12 15:13:47 +0330

    version 1.0

---
+++ cart.py
@@ -0,0 +1,56 @@
+
+from product.models import Product
+
+CART_SESSION_ID = 'cart'
+
+
+class Cart:
+    # برای ساخت سبد خری با سشن از تابع زیر استفاده می کنیم
+    def __init__(self, request):
+        self.session = request.session
+        cart = self.session.get(CART_SESSION_ID)
+        if not cart:
+            cart = self.session[CART_SESSION_ID] = {}
+        self.cart = cart
+    # برای نمایش اجزای داخلی سبد خرید از تابع iter استفاده می کنیم
+    def __iter__(self):
+        cart = self.cart.copy()
+
+        for item in cart.values():
+            product = Product.objects.get(id=int(item['id']))
+            # محصولمون و ایدیشو رو می گیرم و می ریزیم داخل product
+            item['product'] = Product.objects.get(id=int(item['id']))
+            # جمع قیمت ان محصول
+            item['total'] = int(item['quantity']) * int(item['price'])
+            item['unique_id'] = self.unique_id_generator(product.id,item['color'],item['size'])
+            yield item
+
+    def remove_cart(self):
+        del self.session[CART_SESSION_ID]
+
+    def totals(self):
+        cart = self.cart.values()
+        total = 0
+        for item in cart:
+            total += int(item['price']) * int(item['quantity'])
+        return total
+
+    def unique_id_generator(self, id, color, size):
+        result = f'{id}-{color}-{size}'
+        return result
+
+    def add(self, product, quantity, color, size):
+        unique = self.unique_id_generator(product.id, color, size)
+        if unique not in self.cart:
+            self.cart[unique] = {'quantity': 0, 'price': int(product.price), 'color': color, 'size': size,
+                                 'id': str(product.id)}
+        self.cart[unique]['quantity'] += int(quantity)
+        self.save()
+
+    def delete(self, id):
+        if id in self.cart:
+            del self.cart[id]
+            self.save()
+
+    def save(self):
+        self.session.modified = True
